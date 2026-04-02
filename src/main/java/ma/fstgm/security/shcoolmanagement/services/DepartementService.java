da2479078f541fd8974fd5709d006875872216c0
mohamed <m.eddinari5197@uca.ac.ma>
2026-04-02 22:00:21 +0200

    adding departementService

--- src/main/java/ma/fstgm/security/shcoolmanagement/services/DepartementService.java
+++ src/main/java/ma/fstgm/security/shcoolmanagement/services/DepartementService.java
@@ -1,8 +1,14 @@
 package ma.fstgm.security.shcoolmanagement.services;
 
+import ma.fstgm.security.shcoolmanagement.dto.request.DepartementRequest;
+import ma.fstgm.security.shcoolmanagement.dto.response.DepartementResponse;
+import ma.fstgm.security.shcoolmanagement.entities.Departement;
+import ma.fstgm.security.shcoolmanagement.exceptions.ResourceNotFoundException;
 import ma.fstgm.security.shcoolmanagement.mapper.DepartementMapper;
 import ma.fstgm.security.shcoolmanagement.repositories.DepartementRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DepartementService {
@@ -13,4 +19,30 @@ public class DepartementService {
         this.departementRepository = departementRepository ;
         this.departementMapper = departementMapper ;
     }
+    public DepartementResponse addDepartement(DepartementRequest departementRequest){
+        Departement departement = departementMapper.toEntity(departementRequest);
+        Departement addedDepartement = departementRepository.save(departement);
+        return departementMapper.toResponse(addedDepartement);
+
+
+    }
+    public void deleteDepartement(Long id){
+        Departement departement = departementRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Departement with id :  " + id +" not found"));
+        departementRepository.delete(departement);
+    }
+    public DepartementResponse updateDeprtement(Long id, DepartementRequest departementRequest){
+        Departement departement = departementRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Departement with id :  " + id +" not found"));
+        departementRepository.save(departement);
+        return departementMapper.toResponse(departement);
+
+    }
+    public List<DepartementResponse> getAllDepartements(){
+        return departementRepository.findAll()
+                .stream()
+                .map(departementMapper::toResponse)
+                .toList();
+    }
+
 }
